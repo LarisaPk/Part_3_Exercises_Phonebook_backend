eda3c601f1a35030e99f000b4e11a8370e33c179
Larisa Pyykölä <Larisa.pyykola@gmail.com>
2019-10-08 12:45:29 +0300

    frontend

---
+++ README.md
@@ -0,0 +1,2 @@
+link to the application:<br>
+https://fast-lake-00388.herokuapp.com
---
+++ build/precache-manifest.7f44a592ca90da24793255566397cdfc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ba7094880bf2b300fcc83a67959a111",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0bcfb92ace0be63f89f7",
+    "url": "/static/css/main.8b4b54b0.chunk.css"
+  },
+  {
+    "revision": "99fd3e685da2c2fe6225",
+    "url": "/static/js/2.bb044435.chunk.js"
+  },
+  {
+    "revision": "0bcfb92ace0be63f89f7",
+    "url": "/static/js/main.0411604e.chunk.js"
+  },
+  {
+    "revision": "0a3909685390b3afae4a",
+    "url": "/static/js/runtime~main.ce00a3d8.js"
+  }
+]);
